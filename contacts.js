ac5f71260ca9293fb7f567f8ac491ccfd8459ac6
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2024-03-09 22:39:46 +0200

    contacts_js

--- contacts.js
+++ contacts.js
@@ -4,20 +4,48 @@ const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
 async function listContacts() {
-  // ...твій код. Повертає масив контактів.
+  // Повертає масив контактів.
   const data = await fs.readFile(contactsPath);
-
   return JSON.parse(data);
 }
 
 async function getContactById(contactId) {
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+  // Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+  const data = await fs.readFile(contactsPath);
+  const result = data.find((item) => item.id === contactId);
+  return result || null;
 }
 
 async function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  // Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  const data = await fs.readFile(contactsPath);
+  const index = data.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = data.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(data));
+  return result;
 }
 
 async function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту (з id).
+  //Повертає об'єкт доданого контакту (з id).
+  const data = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+    ...data,
+  };
+  data.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(data));
+  return newContact;
 }
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
