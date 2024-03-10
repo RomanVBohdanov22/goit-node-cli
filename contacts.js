618ac4df787a40f011bf9d2f49b2df653baa71da
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2024-03-10 20:24:17 +0200

    ілрщк!

--- contacts.js
+++ contacts.js
@@ -5,7 +5,7 @@ const contactsPath = path.join(__dirname, "db/contacts.json");
 
 async function listContacts() {
   // Повертає масив контактів.
-  const data = await fs.readFile(contactsPath);
+  const data = await fs.readFile(contactsPath, "utf-8");
   return JSON.parse(data);
 }
 
